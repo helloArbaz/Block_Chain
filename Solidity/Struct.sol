aeeadd27c13bfb350dc28d2c1dc9c51471e4f144
Arbaz5 Khan <fc19193@Axis-FC-A00049.local>
2022-05-09 20:29:21 +0530

    working

---
+++ Solidity/Struct.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 < 0.9.0;
+
+struct Student {
+    uint roll;
+    string name;
+}
+
+
+contract Struct { 
+
+    Student public s1;
+
+    constructor(uint _roll , string memory _name){
+        s1.roll = _roll;
+        s1.name = _name;
+    }
+
+    function _change(uint _roll, string memory _name) public {
+        Student memory new_std = Student({
+            roll:_roll,
+            name:_name
+        });
+
+        s1 = new_std;
+    }
+
+}
