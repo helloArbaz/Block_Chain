7570e45800c74cf4c03f115b571ad6f75d65a030
Arbaz5 Khan <fc19193@Axis-FC-A00049.local>
2022-05-10 12:17:50 +0530

    version update

---
+++ SampleSmartContract/VendingMachine.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 < 0.9.0;
+
+// background
+// we can get the VM balance
+// users can purchased the itmes
+// each items is > 1 ether
+// add services charges
+// stockup the values
+
+contract VendingMachine {
+    // init the user wallet address
+    address public owner;
+    mapping(address=>uint) public _VM_Balance;
+
+    constructor() {
+        // assign the owner with the wallet address id
+        owner=msg.sender;
+        _VM_Balance[address(this)] = 100;
+    }
+
+    function checkOwner() public view returns(bool){
+        return msg.sender == owner;
+    }
+
+    function getVMBalance() public view returns(uint){
+        return _VM_Balance[address(this)];
+    }
+
+    function purchased(uint amount) public payable{
+        require(msg.value >= 2 ether , "You Must have at least 2 ether" );
+        require(_VM_Balance[address(this)] >= amount , "Not allowed" );
+        _VM_Balance[address(this)]-=amount;
+        _VM_Balance[owner] +=amount;
+    }
+
+    function getOwnerBalance() public view returns(uint){
+        return owner.balance;
+    }
+
+    function restore(uint _restore) public {
+        require(msg.sender == owner,"Not the owner");
+        _VM_Balance[address(this)] += _restore;
+    }
+
+}
